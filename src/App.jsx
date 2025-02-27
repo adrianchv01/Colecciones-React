7f3d8a55a09a3442b00fa2a698ec37033b545055
adrianchv01 <adrianchv04@gmail.com>
2025-02-26 22:11:59 -0500

    colecciones-array

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-const App = (props) => {
-  const { notes } = props
+import Note from "./components/Note";
 
+const App = ({notes}) => {
+  const result = notes.map(note => note.id);
+  console.log(result);
   return (
     <div>
       <h1>Notes</h1>
+      {/* //De esta forma trae todo de los datos requeridos */}
+      {/* //EL notes.map le envia cada array al note y el note accede al content
+      //y luego devuelve el li */}
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map(note => 
+          <Note key={note.id} note={note}/>)}
       </ul>
+
     </div>
+
   )
 }
 
